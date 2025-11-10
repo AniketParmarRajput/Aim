2f2a453f5a2971d9f626d6da5e387ad26aec54c9
AniketParmarRajput <rajputaniket092@gmail.com>
2025-11-10 12:19:45 +0530

    ther is change

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react'
 
-const page = () => {
+const Page = () => {
   const [form, setForm]=useState({
     name:"",
     email:"",
@@ -37,4 +37,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
