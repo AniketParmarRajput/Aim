864bf08a9f06555d5908c3a72db02be9b77daac6
AniketParmarRajput <rajputaniket092@gmail.com>
2025-11-13 16:29:42 +0530

    new11

--- app/layout.js
+++ app/layout.js
@@ -19,10 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
   );
 }
-
