93dcd2c2f016045a19cb40d9892fbfaeeba0c897
AniketParmarRajput <rajputaniket092@gmail.com>
2025-12-08 20:20:49 +0530

    to learned a usememo hook

--- app/Common/pages/Pricing/page.js
+++ app/Common/pages/Pricing/page.js
@@ -1,11 +1,68 @@
-import React from 'react';
+"use client"
 
-const page = () => {
+import React, { useMemo, useState } from "react"
+
+const Page = () => {
+  const [value, setValue] = useState({
+    number: "",
+    gst: "",
+  })
+
+  const handle1 = (e) => {
+    const { name, value } = e.target
+    setValue((pre) => ({
+      ...pre,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted Values:", value)
+  }
+
+  // Memoized calculation
+  const cal = useMemo(() => {
+    const start = performance.now()
+    console.log("➡️ Calculation started")
+
+    const p = parseFloat(value.number || 0)
+    const g = parseFloat(value.gst || 0)
+
+    const rate = p * g
+
+    const end = performance.now()
+
+
+     console.log(rate)
+     console.log(end);
+  }, [value])
+
   return (
-    <div className='text-red-500'>
-      coming SOON
-    </div>
-  );
+    <>
+      <form className="text-red-500" onSubmit={handleSubmit}>
+        <input
+          name="number"
+          value={value.number}
+          placeholder="number"
+          onChange={handle1}
+        />
+
+        <input
+          name="gst"
+          value={value.gst}
+          placeholder="gst"
+          onChange={handle1}
+        />
+
+        <button type="submit">submit</button>
+        <br />
+
+        {cal}
+      </form>
+    </>
+  )
 }
 
-export default page;
+export default Page
+
